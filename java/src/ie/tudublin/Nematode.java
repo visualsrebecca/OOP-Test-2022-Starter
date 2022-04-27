9d302bcaf595f14adcfcd40c7fc5e1efcd7473ce
Rebecca <C20301121@mytudublin.ie>
2022-04-27 16:47:27 +0100

    generate second constructor

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -1,60 +1,113 @@
 package ie.tudublin;
 
-import java.util.ArrayList;
+
+import processing.core.PApplet;
+import processing.data.TableRow;
+
 
 
 public class Nematode {
     
     private String name;
     private int length;
-    private boolean hasLimbs;
-    private char gender;
+    private boolean limbs;
+    private String gender;
     private boolean eyes;
+
+    
+
+    // creating a toString()
+    @Override
+    public String toString() {
+        return "Nematode [eyes=" + eyes + ", gender=" + gender + ", limbs=" + limbs + ", length=" + length
+                + ", name=" + name + "]";
+    }
 
 
 
     // generating Nematode constrcutor
-    public Nematode(String name, int length, boolean hasLimbs, char gender, boolean eyes) {
+    public Nematode(String name, int length, boolean limbs, String gender, boolean eyes) {
         this.name = name;
         this.length = length;
-        this.hasLimbs = hasLimbs;
+        this.limbs = limbs;
         this.gender = gender;
         this.eyes = eyes;
     }
 
 
+    // creating second constrcutor
+    public Nematode(TableRow tr){
 
-    //creating getters and setters
+        this(
+            tr.getString("name"),
+            tr.getInt("length"),
+            tr.getInt("limbs") == 1,
+            tr.getString("gender"),
+            tr.getInt("eyes") == 1);
+
+    }
+
+
+    //generate getters & setters
     public String getName() {
         return name;
     }
+
+
+
     public void setName(String name) {
         this.name = name;
     }
+
+
+
     public int getLength() {
         return length;
     }
+
+
+
     public void setLength(int length) {
         this.length = length;
     }
-    public boolean isHasLimbs() {
-        return hasLimbs;
+
+
+
+    public boolean isLimbs() {
+        return limbs;
     }
-    public void setHasLimbs(boolean hasLimbs) {
-        this.hasLimbs = hasLimbs;
+
+
+
+    public void setLimbs(boolean limbs) {
+        this.limbs = limbs;
     }
-    public char getGender() {
+
+
+
+    public String getGender() {
         return gender;
     }
-    public void setGender(char gender) {
+
+
+
+    public void setGender(String gender) {
         this.gender = gender;
     }
+
+
+
     public boolean isEyes() {
         return eyes;
     }
+
+
+
     public void setEyes(boolean eyes) {
         this.eyes = eyes;
     }
+
+
 
 
 
